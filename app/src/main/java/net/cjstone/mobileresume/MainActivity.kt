d4b43338a60e81b29632f49e289e4d540f7916a0
Christopher Stone <christopher@closetoeuphoria.com>
2018-05-09 21:57:31 -0400

    Created new Work History activity

--- app/src/main/java/net/cjstone/mobileresume/MainActivity.kt
+++ app/src/main/java/net/cjstone/mobileresume/MainActivity.kt
@@ -1,12 +1,21 @@
 package net.cjstone.mobileresume
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val workHistory = findViewById<Button>(R.id.work_history)
+
+        workHistory.setOnClickListener {
+            val moveToWorkHistory = Intent(applicationContext, WorkHistoryActivity::class.java)
+            startActivity(moveToWorkHistory)
+        }
     }
 }
