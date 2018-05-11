99dcf0c44c8fd86ef8ae51ccc4b209008f71d35a
Christopher Stone <christopher@closetoeuphoria.com>
2018-05-10 23:57:18 -0400

    All actions on main screen are now working

--- app/src/main/java/net/cjstone/mobileresume/MainActivity.kt
+++ app/src/main/java/net/cjstone/mobileresume/MainActivity.kt
@@ -1,6 +1,7 @@
 package net.cjstone.mobileresume
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
@@ -12,10 +13,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val workHistory = findViewById<Button>(R.id.work_history)
-
         workHistory.setOnClickListener {
             val moveToWorkHistory = Intent(applicationContext, WorkHistoryActivity::class.java)
             startActivity(moveToWorkHistory)
+        }
+
+        val call = findViewById<Button>(R.id.call)
+        call.setOnClickListener {
+            // Not committing my actual phone number here.
+            val phoneUri = Uri.parse("tel:15555555555")
+            val callIntent = Intent(Intent.ACTION_DIAL, phoneUri)
+            startActivity(callIntent)
+        }
+
+        val email = findViewById<Button>(R.id.email)
+        email.setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SEND)
+            emailIntent.type = "plain/text"
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("christopher@closetoeuphoria.com"))
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Contact through Mobile Resume")
+            emailIntent.putExtra(Intent.EXTRA_TEXT, "Hello,\n\n" +
+                    "I saw your resume and would like to get in touch.\n\n")
+            startActivity(emailIntent)
         }
     }
 }
